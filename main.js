32c3809c0454b58ddb50598773f8f6c0bae8502d
funhousekush <85101915+funhousekush@users.noreply.github.com>
2021-08-12 20:37:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+leftwristX = 0;
+leftwristY = 0;
+
+rightwristX = 0;
+rightwristY = 0;
+
 function preload()
 {
     the_untold_story = "the untold story.mp3";
@@ -25,6 +31,14 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+
+        leftwristX = results[0].pose.leftWrist.x;
+        leftwristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftwristX + "leftWristY = " + leftwristY);
+
+        rightwristX = results[0].pose.rightWrist.x;
+        rightwristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightwristX + "rightWristY = " + rightwristY);
     }
 }
 
